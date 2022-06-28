fc022eea620910d89308628ef64a6759a560e399
LucasPessivaldo <11798065+lucaspessivaldo@users.noreply.github.com>
2022-06-27 22:31:38 -0300

    Update - change Header to Navbar

---
+++ src/components/navBar/NavBar.tsx
@@ -0,0 +1,11 @@
+import './navbar.css'
+
+export default function NavBar() {
+  return (
+    <header>
+      <div className='header_title'>Unsplash ReactJS</div>
+      <nav>
+      </nav>
+    </header>
+  )
+}
